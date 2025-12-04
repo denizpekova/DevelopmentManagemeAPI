c21deebe107f5dbce28fd2d2ea40b38c5cf29b24
Faxror <denizpekova9@gmail.com>
2025-12-05 02:17:16 +0300

    add program.cs rules

--- MTAPI/Program.cs
+++ MTAPI/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
